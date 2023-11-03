784a835546d269365251719e2796c4b77aae8191
likenesio <valentinubb@gmail.com>
2023-11-03 02:24:54 -0300

    [separación por componentes de cada sección del portafolio]

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NavbarComponent,
+    PresentacionComponent,
+    ProyectosComponent,
+    AptitudesComponent,
+    FormacionComponent,
   ],
   imports: [
     BrowserModule,
